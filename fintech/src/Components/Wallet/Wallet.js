2af225b1d3e4b22e9758e8d71236cd2a3200c2bc
Rayan ALi <rayan.ali.8205@gmail.com>
2023-12-01 13:00:43 +0200

    Wallet page with no responsive

---
+++ fintech/src/Components/Wallet/Wallet.js
@@ -0,0 +1,127 @@
+import React from 'react'
+import styles from './Wallet.module.css'
+import WalletCards from '../../Components/WalletCards/WalletCards';
+import crypto from '../../assets/images/crypto.png'
+import dollar from '../../assets/images/dollar.png'
+import Button from '@mui/material/Button';
+
+export default function Wallet() {
+    const useStyle = {
+        hoverBtn: {
+            background: `#FFE712`,
+            color: "#000",
+            height: `3em`,
+            width: `25%`,
+            '&:hover': {
+                backgroundColor: `#ffef61 !important`
+            },
+        },
+    }
+  return (
+    <div className={styles.walletHolder}>
+    <div className={styles.nbDiv}>
+                        <section className={styles.balanceHolder}>
+
+                            <article className={styles.actions}>
+                                <div className={styles.balanceCurrencySpan}>
+                                    <h2 className={`${styles.walletTitles} ${styles.titleInline}`}>Total Balance</h2>
+                                    <span className={styles.currency}>USD</span>
+                                </div>
+                                <div className={styles.balanceSpan}>
+                                    <span>$2578.55</span>
+                                </div>
+                                <div className={styles.actionBtns}>
+                                    {/* <span className={`${styles.actionbtn} ${styles.depositBtn}`}>Deposit</span>
+                                    <span className={styles.actionbtn}>Withdraw</span>
+                                    <span className={styles.actionbtn}>Transfer</span> */}
+                                    <Button variant="contained" sx={useStyle.hoverBtn}>Deposit</Button>
+                                    <Button variant="contained" sx={{ background: `rgba(40, 87, 254, 0.48)`, height: `3em`, width: `25%` }}   >Withdraw</Button>
+                                    <Button variant="contained" sx={{ background: `rgba(40, 87, 254, 0.48)`, height: `3em`, width: `25%` }} >     Transfer
+                                    </Button>
+
+                                </div>
+                            </article>
+
+                            <article className={styles.balances}>
+                                <h2 className={`${styles.walletTitles} ${styles.balanceTitle}`}>Balances</h2>
+                                <div className={styles.usdtBalance}>
+                                    <span className={styles.cuurencyLogo} >
+                                        <img src={crypto} alt="crypto Logo" className={styles.cryptoLogo}></img>
+                                        USDT
+                                    </span>
+                                    <span className={styles.cuurencyAmount} >
+                                        1500.87
+                                    </span>
+
+                                </div>
+                                <div className={styles.usdBalance}>
+                                    <span className={styles.cuurencyLogo} >
+                                        <img src={dollar} alt="dollar Logo" className={styles.dollarLogo}></img>
+                                        USD
+                                    </span>
+                                    <span className={styles.cuurencyAmount} >
+                                        1500.87
+                                    </span>
+                                </div>
+                            </article>
+
+                        </section>
+
+                        <section className={styles.recentHolder}>
+                            <article className={styles.recentTitle}>
+                                <span>Wallet Activity</span>
+                                <span>Recent</span>
+
+                            </article>
+                            <article className={styles.recents}>
+                                <div className={styles.recentOne}>
+                                    <span className={styles.recentArrow}>X</span>
+                                    <span>withdraw</span>
+                                    <span>23:10:45</span>
+                                    <span>$44</span>
+                                    <span>pending</span>
+                                </div>
+
+                                <div className={styles.recentOne}>
+                                    <span className={styles.recentArrow}>X</span>
+                                    <span>withdraw</span>
+                                    <span>23:10:45</span>
+                                    <span>$44</span>
+                                    <span>pending</span>
+                                </div>
+
+                                <div className={styles.recentOne}>
+                                    <span className={styles.recentArrow}>X</span>
+                                    <span>withdraw</span>
+                                    <span>23:10:45</span>
+                                    <span>$44</span>
+                                    <span>pending</span>
+                                </div>
+
+                                <div className={styles.recentOne}>
+                                    <span className={styles.recentArrow}>X</span>
+                                    <span>withdraw</span>
+                                    <span>23:10:45</span>
+                                    <span>$44</span>
+                                    <span>pending</span>
+                                </div>
+
+                                <div className={styles.recentOne}>
+                                    <span className={styles.recentArrow}>X</span>
+                                    <span>withdraw</span>
+                                    <span>23:10:45</span>
+                                    <span>$44</span>
+                                    <span>pending</span>
+                                </div>
+
+                            </article>
+                        </section>
+                    </div>
+                    <div className={styles.chartsHolder} >
+                        <WalletCards />
+                        <WalletCards />
+                        <WalletCards />
+                    </div>
+    </div>
+  )
+}
