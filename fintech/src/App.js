ea0d51d551e836b1b588f4a2986e8b19d1855472
Fuad Abou Heit <f.abouheit@gmail.com>
2023-12-01 16:10:17 +0200

    finished transaction component

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,12 +1,29 @@
 
+import { purple } from '@mui/material/colors';
 import './App.css';
-import TransactionCard from './Components/TransactionCard/TransactionCard.jsx';
+import Transaction from './Components/TransactionCard/Transaction';
+// import TransactionCard from './Components/TransactionCard/TransactionCard.jsx';
+import { ThemeProvider, createTheme,  } from '@mui/material';
 
 function App() {
+
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main: "rgba(255,255,255,0.2)",
+        
+      },
+      secondary:{ 
+        main: "#110A3A"
+      },
+    }
+  })
   return (
+      <ThemeProvider theme={theme}>
       <div>
-        <TransactionCard/>
+        <Transaction/>
       </div>
+      </ThemeProvider>
    
   );
 }
