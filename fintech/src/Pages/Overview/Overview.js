e2bba0ede347a937c0b265503c95f32fdddbce70
Rayan ALi <rayan.ali.8205@gmail.com>
2023-12-07 09:21:37 +0200

    merchant overview

--- fintech/src/Pages/Overview/Overview.js
+++ fintech/src/Pages/Overview/Overview.js
@@ -9,6 +9,7 @@ import BasicColor from '../../Components/Charts/Charts';
 import HiddenLegend from '../../Components/charts2/CircularChart';
 // import recentMerchant from '../../Components/recentMerchant/recentMerchant';
 import RecentMerchant from '../../Components/recentMerchant/recentMerchant';
+import DashCards from '../../Components/dashCards/DashCards'
 const sample = [1, 10, 30, 50, 70, 90, 100];
 
 const seriesA = {
@@ -17,8 +18,12 @@ const seriesA = {
 };
 
 export default function BasicStacking() {
+  const [role,setRole]=React.useState("merchant")
   return (
-    <div style={{margin:"0 auto",border:"1px solid white",width:"100%",height:"90%"}} className={Styles.overviewHolder}>
+    <>
+    {
+      role==="merchant"?
+      (    <div style={{margin:"0 auto",border:"1px solid white",width:"100%",height:"90%"}} className={Styles.overviewHolder}>
       {/* <div >
     <div style={{display:"flex", flexDirection:"row", gap:"2rem"}}>
     <BarChart
@@ -37,13 +42,34 @@ export default function BasicStacking() {
     
     </div>   */}
 <div className={Styles.merchantflex1}>
-hii
+<div className={Styles.chartMerchant}>
+
+</div>
+<div className={Styles.cardsMerchants}>
+  <div className={Styles.cardMerchant}>
+  <DashCards />
+
+  </div>
+  <div className={Styles.cardMerchant}>
+  <DashCards />
+
+  </div>
+</div>
 </div>
 <div className={Styles.merchantflex2}>
 <RecentMerchant/>
 
 </div>
-    </div>
+    </div>)
+      :
+      (
+        <div style={{width:"100%",height:"100%"}}>
+Helloo
+        </div>
+      )
+    }
+    </>
+
     
   );
 }
