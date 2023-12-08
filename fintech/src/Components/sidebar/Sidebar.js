4fe6f9a75a7cd9b63ec958440e0f7bea851bd1ad
Rayan ALi <rayan.ali.8205@gmail.com>
2023-12-08 09:23:25 +0200

    side bar conditions

--- fintech/src/Components/sidebar/Sidebar.js
+++ fintech/src/Components/sidebar/Sidebar.js
@@ -21,104 +21,221 @@ import './sidebar.module.css';
 import {Link} from "react-router-dom"
 import { useState } from 'react';
 import { flushSync } from 'react-dom';
+import PendingIcon from '@mui/icons-material/Pending';
 
 
 const drawerWidth = 240;
 
 export default function ClippedDrawer() {
   const [ activeSide, setActiveSide]= useState("Dashboard")
+  const [role,setRole]=useState('admin')
   const handleClick=(filter)=>{
-    flushSync(()=>{
-      setActiveSide(filter)
-    })
+    setActiveSide(filter)
+    // flushSync(()=>{
+    //   setActiveSide(filter)
+    // })
   }
 
   return (
-    <Box className={Style.box} sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Drawer
-        variant="permanent"
-        sx={{
+    <>
+    {role!=="admin"?
+    
+    (    <Box className={Style.box} sx={{ display: 'flex' }}>
+    <CssBaseline />
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
           width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            backgroundColor: '#0C0E2E',
-          },
-        }}
-      >
-        <Toolbar>
-          <div style={{  width: '100%', height: '50%', display: 'flex',margin:"20px auto", alignItems: 'center',padding:"0px", justifyContent: 'center', marginLeft:"-10px", color:"#a0a9a2", fontFamily:"Rubik, sans-serif" , fontSize:"25px",fontWeight:"bold"}}>
-            Digital Wallet
-          </div>
-        </Toolbar>
-        <Box sx={{ overflow: 'auto', flexDirection: 'column' }}>
-          <List sx={{ display: "flex", flexDirection: "column", gap: '1rem', color:"white" , }}>
-            <ListItem disablePadding>
-            <Link to='/' style={{textDecoration:"none", color:"white", }}>
-              <ListItemButton key='Dashboard' className={`${Style.activee} ${activeSide === "Dashboard" ?  "active":""}`} onClick={()=> handleClick('Dashboard')} sx={{width:"125%"}} >
-                
-                <ListItemIcon>
-                  <DashboardIcon sx={{color:"white"}}/>
-                </ListItemIcon>
-                
-                <ListItemText primary="Dashboard"/>
-                
-              </ListItemButton>
-              </Link>
-            </ListItem>
+          boxSizing: 'border-box',
+          backgroundColor: '#0C0E2E',
+        },
+      }}
+    >
+      <Toolbar>
+        <div style={{  width: '100%', height: '50%', display: 'flex',margin:"20px auto", alignItems: 'center',padding:"0px", justifyContent: 'center', marginLeft:"-10px", color:"#a0a9a2", fontFamily:"Rubik, sans-serif" , fontSize:"25px",fontWeight:"bold"}}>
+          Digital Wallet
+        </div>
+      </Toolbar>
+      <Box sx={{ overflow: 'auto', flexDirection: 'column' }}>
+        <List sx={{ display: "flex", flexDirection: "column", gap: '1rem', color:"white" , }}>
+          <ListItem disablePadding>
+          <Link to='/' style={{textDecoration:"none", color:"white", }}>
+            <ListItemButton key='Dashboard' className={`${Style.activee} ${activeSide === "Dashboard" ? Style.active:""}`} onClick={()=> handleClick('Dashboard')} sx={{width:"125%"}} >
+              
+              <ListItemIcon>
+                <DashboardIcon sx={{color:"white"}}/>
+              </ListItemIcon>
+              
+              <ListItemText primary="Dashboard"/>
+              
+            </ListItemButton>
+            </Link>
+          </ListItem>
 
-            <ListItem disablePadding>
-            <Link to='transaction' style={{textDecoration:"none", color:"white"}}>
-              <ListItemButton key="Transaction" className={`${Style.activee} ${activeSide === "Transaction" ?  "active":""}`} onClick={()=> handleClick('Transaction')} sx={{width:"120%" }}>
-                <ListItemIcon>
-                  <AccountBalanceWalletIcon sx={{color:"white"}}/>
-                </ListItemIcon>
-                <ListItemText primary="Transaction" />
-              </ListItemButton>
-              </Link>
-            </ListItem>
+          <ListItem disablePadding>
+          <Link to='transaction' style={{textDecoration:"none", color:"white"}}>
+            <ListItemButton key="Transaction" className={`${Style.activee} ${activeSide === "Transaction" ?  "active":""}`} onClick={()=> handleClick('Transaction')} sx={{width:"120%" }}>
+              <ListItemIcon>
+                <AccountBalanceWalletIcon sx={{color:"white"}}/>
+              </ListItemIcon>
+              <ListItemText primary="Transactions" />
+            </ListItemButton>
+            </Link>
+          </ListItem>
+{
+role==="merchant"?
+(<ListItem disablePadding>
+  <Link to='/transaction' style={{textDecoration:"none" , color:"white"}}>
+    <ListItemButton sx={{width:"140%"}}>
+      <ListItemIcon>
+<PendingIcon sx={{color:"white"}} />
+        {/* <CurrencyExchangeIcon  sx={{color:"white"}} /> */}
+      </ListItemIcon>
+      <ListItemText primary="Requests" />
+    </ListItemButton>
+    </Link>
+  </ListItem>)
+:
+(<ListItem disablePadding>
+  <Link to='/transaction' style={{textDecoration:"none" , color:"white"}}>
+    <ListItemButton sx={{width:"140%"}}>
+      <ListItemIcon>
+        <CurrencyExchangeIcon  sx={{color:"white"}}/>
+      </ListItemIcon>
+      <ListItemText primary="Transfer" />
+    </ListItemButton>
+    </Link>
+  </ListItem>)
+}
+          
 
-            <ListItem disablePadding>
-            <Link to='/transaction' style={{textDecoration:"none" , color:"white"}}>
-              <ListItemButton sx={{width:"140%"}}>
-                <ListItemIcon>
-                  <CurrencyExchangeIcon  sx={{color:"white"}}/>
-                </ListItemIcon>
-                <ListItemText primary="Transfer" />
-              </ListItemButton>
-              </Link>
-            </ListItem>
+          <ListItem disablePadding>
+          <Link to='/wallet' style={{textDecoration:"none", color:"white"}}>
+            <ListItemButton sx={{width:"130%"}}>
+              <ListItemIcon>
+                <WalletIcon  sx={{color:"white"}} />
+              </ListItemIcon>
+              <ListItemText primary="My Wallet" />
+            </ListItemButton>
+            </Link>
+          </ListItem>
+        </List>
 
-            <ListItem disablePadding>
-            <Link to='/wallet' style={{textDecoration:"none", color:"white"}}>
-              <ListItemButton sx={{width:"130%"}}>
-                <ListItemIcon>
-                  <WalletIcon  sx={{color:"white"}} />
-                </ListItemIcon>
-                <ListItemText primary="My Wallet" />
-              </ListItemButton>
-              </Link>
-            </ListItem>
-          </List>
+        <Divider />
 
-          <Divider />
+        <List sx={{ display: "flex", flexDirection: "column", gap: '1rem' }}>
+          <ListItem disablePadding>
+          <Link to='/profile' style={{textDecoration:"none", color:"white"}}>
+            <ListItemButton sx={{width:"150%"}}>
+              <ListItemIcon>
+                <ManageAccountsIcon  sx={{color:"white"}}/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+            </Link>
+          </ListItem>
+        </List>
+      </Box>
+    </Drawer>
+  </Box>)
+  :
 
-          <List sx={{ display: "flex", flexDirection: "column", gap: '1rem' }}>
-            <ListItem disablePadding>
-            <Link to='/profile' style={{textDecoration:"none", color:"white"}}>
-              <ListItemButton sx={{width:"150%"}}>
-                <ListItemIcon>
-                  <ManageAccountsIcon  sx={{color:"white"}}/>
-                </ListItemIcon>
-                <ListItemText primary="Profile" />
-              </ListItemButton>
-              </Link>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+
+  (  
+    
+    
+    <Box className={Style.box} sx={{ display: 'flex' }}>
+  <CssBaseline />
+  <Drawer
+    variant="permanent"
+    sx={{
+      width: drawerWidth,
+      flexShrink: 0,
+      [`& .MuiDrawer-paper`]: {
+        width: drawerWidth,
+        boxSizing: 'border-box',
+        backgroundColor: '#0C0E2E',
+      },
+    }}
+  >
+    <Toolbar>
+      <div style={{  width: '100%', height: '50%', display: 'flex',margin:"20px auto", alignItems: 'center',padding:"0px", justifyContent: 'center', marginLeft:"-10px", color:"#a0a9a2", fontFamily:"Rubik, sans-serif" , fontSize:"25px",fontWeight:"bold"}}>
+        Digital Wallet
+      </div>
+    </Toolbar>
+    <Box sx={{ overflow: 'auto', flexDirection: 'column' }}>
+      <List sx={{ display: "flex", flexDirection: "column", gap: '1rem', color:"white" , }}>
+        <ListItem disablePadding>
+        <Link to='/' style={{textDecoration:"none", color:"white", }}>
+          <ListItemButton key='Dashboard' className={`${Style.activee} ${activeSide === "Dashboard" ? Style.active:""}`} onClick={()=> handleClick('Dashboard')} sx={{width:"125%"}} >
+            
+            <ListItemIcon>
+              <DashboardIcon sx={{color:"white"}}/>
+            </ListItemIcon>
+            
+            <ListItemText primary="Dashboard"/>
+            
+          </ListItemButton>
+          </Link>
+        </ListItem>
+
+     
+        <ListItem disablePadding>
+        <Link to='/wallet' style={{textDecoration:"none", color:"white"}}>
+          <ListItemButton sx={{width:"130%"}}>
+            <ListItemIcon>
+              <WalletIcon  sx={{color:"white"}} />
+            </ListItemIcon>
+            <ListItemText primary="Admins" />
+          </ListItemButton>
+          </Link>
+        </ListItem>
+        
+        <ListItem disablePadding>
+        <Link to='/wallet' style={{textDecoration:"none", color:"white"}}>
+          <ListItemButton sx={{width:"130%"}}>
+            <ListItemIcon>
+              <WalletIcon  sx={{color:"white"}} />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+          </Link>
+        </ListItem>
+
+        <ListItem disablePadding>
+        <Link to='transaction' style={{textDecoration:"none", color:"white"}}>
+          <ListItemButton key="Transaction" className={`${Style.activee} ${activeSide === "Transaction" ?  "active":""}`} onClick={()=> handleClick('Transaction')} sx={{width:"120%" }}>
+            <ListItemIcon>
+              <AccountBalanceWalletIcon sx={{color:"white"}}/>
+            </ListItemIcon>
+            <ListItemText primary="Transactions" />
+          </ListItemButton>
+          </Link>
+        </ListItem>
+
+      </List>
+
+      <Divider />
+
+      <List sx={{ display: "flex", flexDirection: "column", gap: '1rem' }}>
+        <ListItem disablePadding>
+        <Link to='/profile' style={{textDecoration:"none", color:"white"}}>
+          <ListItemButton sx={{width:"150%"}}>
+            <ListItemIcon>
+              <ManageAccountsIcon  sx={{color:"white"}}/>
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+          </Link>
+        </ListItem>
+      </List>
     </Box>
+  </Drawer>
+</Box>)}
+    </>
+
   );
 }
